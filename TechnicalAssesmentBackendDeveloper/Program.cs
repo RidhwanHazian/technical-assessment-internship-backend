a4eeacd75be936708baecc087ed94cba81984d29
RidhwanHazian <iwghostpride@gmail.com>
2025-07-08 18:59:55 +0800

    Successfully introduce new class Fruit and add using ItemManager<Fruit>, done part three

--- TechnicalAssesmentBackendDeveloper/Program.cs
+++ TechnicalAssesmentBackendDeveloper/Program.cs
@@ -16,8 +16,15 @@
         // Part Two: Implement the RemoveItem method
         manager.RemoveItem("Apple");
 
+
+
         // Part Three: Introduce a Fruit class and use the ItemManager<Fruit> to add a few fruits and print them on the console.
         // TODO: Implement this part three.
+        ItemManager<Fruit> fManager = new ItemManager<Fruit>();
+        fManager.AddItem(new Fruit("Durian"));
+        fManager.AddItem(new Fruit("Rambutan"));
+        fManager.AddItem(new Fruit("Dates"));
+        fManager.PrintAllItems();
 
         // Part Four (Bonus): Implement an interface IItemManager and make ItemManager implement it.
         // TODO: Implement this part four.
@@ -94,5 +101,21 @@ public class ItemManager<T>
     public void ClearAllItems()
     {
         items = new List<T>();
+    }
+}
+
+// Part Three: Introduce a Fruit class and use the ItemManager<Fruit> to add a few fruits and print them on the console.
+public class Fruit
+{
+    public string Name { get; set; }
+
+    public Fruit(string name)
+    {
+        Name = name;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}";
     }
 }
